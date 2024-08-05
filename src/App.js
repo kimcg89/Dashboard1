43c5190e231b4df80ee34199594c799ad241347f
kimcg89 <kimcg891@gmail.com>
2024-08-05 17:37:43 +0900

    Topbar Sidebar Team

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import Topbar from "./scences/global/Topbar";
-import Dashboard from "./scences/dashboard";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import {Routes, Route } from "react-router-dom"
+import Topbar from "./scences/global/Topbar";
+import Sidebar from "./scences/global/Sidebar";
+import Dashboard from "./scences/dashboard";
+import Team from "./scences/team";
+// import Invoices from "./scences/invoices";
+// import Contacts from "./scences/contacts";
+// import Bar from "./scences/bar";
+// import Form from "./scences/form";
+// import Line from "./scences/line";
+// import Pie from "./scences/pie";
+// import FAQ from "./scences/faq";
+// import Geography from "./scences/geography";
+// import Calendar from "./scences/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -12,8 +23,23 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/form" element={<Form />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/faq" element={<FAQ />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
+              {/* <Route path="/calendar" element={<Calendar />} /> */}
+
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
--- src/scences/dashboard/index.jsx
+++ src/scences/dashboard/index.jsx
@@ -1,5 +1,20 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+// import { useTheme } from "@emotion/react";
+// import { tokens } from "../../theme";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  // const theme = useTheme();
+  // const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      {/* HEADER */}
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
+  );
 };
 
 export default Dashboard;
